decd53a6741f17014ae9c40693933592f4eef665
Kanueven <raelrakeli@gmail.com>
2024-08-15 20:01:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/marsphotos/screens/HomeScreen.kt
@@ -0,0 +1,42 @@
+package com.example.marsphotos.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.marsphotos.R
+import com.example.marsphotos.ui.theme.MarsPhotosTheme
+
+@Composable
+fun HomeScreen(
+    marsUiState: String,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+){
+    ResultScreen(photos = marsUiState, modifier = modifier.padding(contentPadding.calculateTopPadding()))
+
+}
+
+@Composable
+fun ResultScreen(photos: String,modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier)
+    {
+        Text(text = photos)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ResultScreenPreview() {
+    MarsPhotosTheme {
+        ResultScreen(stringResource(R.string.placeholder_result))
+    }
+}
